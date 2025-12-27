fc03a32add4e49bd21b74fb605be39c073940ac6
KwaHash <cosmosray001@gmail.com>
2025-12-27 14:41:27 +0900

    fix: auth pages layout

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,5 @@
 import SignUpPage from '@/features/auth/sign-up'
 
 export default function page() {
-  return (
-    <main className="flex w-full min-h-screen grow bg-[#eee]">
-      <SignUpPage />
-    </main>
-  )
+  return <SignUpPage />
 }
--- app/(auth)/verify/page.tsx
+++ app/(auth)/verify/page.tsx
@@ -1,9 +1,5 @@
 import VerifyPage from '@/features/auth/token-verify'
 
 export default function page() {
-  return (
-    <main className="flex w-full min-h-screen grow bg-[#eee]">
-      <VerifyPage />
-    </main>
-  )
+  return <VerifyPage />
 }
