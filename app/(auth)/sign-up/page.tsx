d3c4137db39ab4901f7582b8bbfb569deb625614
KwaHash <cosmosray001@gmail.com>
2025-12-27 14:07:29 +0900

    feat: sign-up page

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpPage from '@/features/auth/sign-up'
+
+export default function page() {
+  return (
+    <main className="flex w-full min-h-screen grow bg-[#eee]">
+      <SignUpPage />
+    </main>
+  )
+}
