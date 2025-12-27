a9982305bdaf323456be889c871cf36ea9d1d7d9
KwaHash <cosmosray001@gmail.com>
2025-12-27 14:10:56 +0900

    feat: token-verify page

---
+++ app/(auth)/verify/page.tsx
@@ -0,0 +1,9 @@
+import VerifyPage from '@/features/auth/token-verify'
+
+export default function page() {
+  return (
+    <main className="flex w-full min-h-screen grow bg-[#eee]">
+      <VerifyPage />
+    </main>
+  )
+}
