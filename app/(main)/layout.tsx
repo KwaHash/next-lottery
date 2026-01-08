d9c6d7b1ec53c927255b5377c1bb92e3599d8c42
KwaHash <cosmosray001@gmail.com>
2026-01-09 00:31:50 +0900

    feat: bottom-menu

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import MainHeader from "@/components/header/main-header"
+import BottomMenu from "@/components/bottom-menu"
 
 export default function AuthLayout({
   children,
@@ -8,9 +9,10 @@ export default function AuthLayout({
   return (
     <>
       <MainHeader />
-      <main className="flex w-full grow bg-[#eee] min-h-[calc(100dvh_-_88px)]">
+      <main className="flex w-full grow bg-[#eee] min-h-[calc(100dvh_-_88px)] mb-[78px] sm:mb-0">
         {children}
       </main>
+      <BottomMenu />
     </>
   )
 }
---
+++ components/bottom-menu.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { GiTrophy } from "react-icons/gi"
+import { FaListCheck } from "react-icons/fa6"
+import { FaLine } from "react-icons/fa"
+import { BiSolidPurchaseTag } from "react-icons/bi"
+
+const BottomMenu = () => {
+  const pathname = usePathname()
+  const MenuItems = [
+    { label: '当選情報', icon: GiTrophy, href: '/performance' },
+    { label: 'ポイント購入', icon: BiSolidPurchaseTag, href: '/point-purchase' },
+    { label: 'プラン一覧', icon: FaListCheck, href: '/' },
+    { label: 'お問合せ', icon: FaLine, href: '/contact' },
+  ];
+
+  return (
+    <div className="block sm:hidden fixed bottom-0 left-0 right-0">
+      <div className="flex items-center justify-around border-t border-gray-300 bg-gray-100 py-4">
+        {MenuItems.map((item) => {
+          const isActive = pathname.startsWith(item.href);
+          return (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="relative flex flex-col gap-1 items-center justify-center"
+            >
+              <item.icon
+                className="h-6 w-6"
+                style={isActive ? { fill: "#997b35" } : { fill: "#6b7280" }}
+              />
+              <span className={`text-xs font-bold ${isActive ? "text-m-gold" : "text-gray-700"}`}>{item.label}</span>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default BottomMenu
