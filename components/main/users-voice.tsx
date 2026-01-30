eb85b1fa31a2967c34b0634b8930c113a4f3c070
KwaHash <cosmosray001@gmail.com>
2026-01-30 17:19:06 +0900

    fix: user-voice and non-member image

--- components/main/users-voice.tsx
+++ components/main/users-voice.tsx
@@ -6,10 +6,19 @@ const UsersVoice = () => {
       <h2 className='text-4xl sm:text-5xl text-m-gold text-center font-bold drop-shadow-[1px_1px_1px_rgba(0,0,0,0.3)]'>会員様の声</h2>
       <div className='flex flex-col gap-6 mt-8'>
         <figure className='w-full'>
-          <Image src="/imgs/voice_01.png" alt="ミリオンゲート" width={768} height={768} className='mx-auto' priority />
+          <Image src="/imgs/voice/voice_01.png" alt="ミリオンゲート" width={768} height={768} className='mx-auto' priority />
         </figure>
         <figure className='w-full'>
-          <Image src="/imgs/voice_02.png" alt="ミリオンゲート" width={768} height={768} className='mx-auto' priority />
+          <Image src="/imgs/voice/voice_02.png" alt="ミリオンゲート" width={768} height={768} className='mx-auto' priority />
+        </figure>
+        <figure className='w-full'>
+          <Image src="/imgs/voice/voice_03.png" alt="ミリオンゲート" width={768} height={768} className='mx-auto' priority />
+        </figure>
+        <figure className='w-full'>
+          <Image src="/imgs/voice/voice_04.png" alt="ミリオンゲート" width={768} height={768} className='mx-auto' priority />
+        </figure>
+        <figure className='w-full'>
+          <Image src="/imgs/voice/voice_05.png" alt="ミリオンゲート" width={768} height={768} className='mx-auto' priority />
         </figure>
       </div>
     </>
